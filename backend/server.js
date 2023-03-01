396ab05b012b36d1fd56fad2acf01010478f2a3a
Adham <adhamezaldin@gmail.com>
2023-03-01 02:12:12 +0200

    first commit

--- backend/server.js
+++ backend/server.js
@@ -3,6 +3,8 @@ require('dotenv').config()
 const express = require('express')
 const mongoose = require('mongoose')
 const tasksRoutes = require('./routes/tasks')
+const userRoutes = require('./routes/user')
+
 
 mongoose.set('strictQuery', true);
 
@@ -17,6 +19,7 @@ app.use((req,res,next) => {
 
 
 app.use('/api/tasks',tasksRoutes)
+app.use('/api/user',userRoutes)
 
 mongoose.connect(process.env.MONGO_URI)
     .then(() =>{
